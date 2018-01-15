0f482fc3c9c200fa3616805a4f4bbe829b816c77
Allen Kim <allen.kim@rci.rogers.ca>
2018-01-15 12:29:04 -0500

    fixed package.json finding bug

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-var packpath = require('packpath');
 var path = require('path');
 
 //<-- your own confile dir
-var configDir = path.join(packpath.self(), 'config');
 var defaultConfig = require(__dirname + '/default-configs/config.json');
 defaultConfig.customUrls = __dirname + '/default-configs/custom-urls.json';
 defaultConfig.proxyUrls  = __dirname + '/default-configs/proxy-urls.json';
@@ -11,7 +9,8 @@ defaultConfig.https = {
     cert: __dirname + '/default-configs/ssl.crt'
   };
 
-var packageJson = require(path.join(packpath.self(), 'package.json'));
+// to get the current directory package.json
+var packageJson = require(path.resolve(path.join('.', 'package.json')));
 var userConfig = packageJson.httpRequestMiddleware;
 
 var config = Object.assign({}, defaultConfig, userConfig);
