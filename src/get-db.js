ea3b6e36121b382d0a176bd5c3f136ce209c4d94
Allen Kim <allenhwkim@gmail.com>
2018-09-04 12:13:57 -0400

    Update get-db.js

--- src/get-db.js
+++ src/get-db.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const sqlite3 = require('better-sqlite3');
-const mockResponsesDBPath = path.join(process.cwd(), 'mock-responses.sqlite3');
+const glob = require('glob');
+
+let dbFile = glob.sync('{,!(node_modules|dist|coverage)/**/}/mock-responses.sqlite3')[0];
+let mockResponsesDBPath = path.join(process.cwd(), dbFile);
+console.info('mock-responses is using sqlite3 db', mockResponsesDBPath);
 
 module.exports = {
   get path() {
     return mockResponsesDBPath;
   },
   get sqlite3() {
-    return new sqlite3(mockResponsesDBPath);
+    return new sqlite3(this.path);
   }
 }
