98555a64c67510c31beaa2573d243b66c9bb41bd
Allen Kim <allenhwkim@gmail.com>
2019-10-04 17:27:41 -0400

    added server.sh and kill-port module

---
+++ server.sh
@@ -0,0 +1,7 @@
+set -x
+node_modules/.bin/kill-port 9300
+nohup ts-node -r tsconfig-paths/register src/main.ts \
+  --db-path=./mock-responses.sql \
+  --port=9300 \
+  --ssl \
+  --cookie='PLAY_SESSION=ACCTNBR=123456789; Path=/' &
